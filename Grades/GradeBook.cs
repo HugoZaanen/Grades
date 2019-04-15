395280c3a9e52841268c3408ded0b4b6665b7b16
Huego <hugozaanen@hotmail.com>
2019-04-15 11:56:47 +0200

    GradeBook and UnitTesting

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    internal class GradeBook
+    {
+        List<float> Grades;
+
+        public GradeBook()
+        {
+            Grades = new List<float>();
+        }
+
+        internal void AddGrade(float fl)
+        {
+            Grades.Add(fl);
+        }
+
+        //internal GradeStatistics ComputeStatistics()
+        //{
+            
+        //}
+    }
+}
---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,45 @@
+﻿namespace Grades
+{
+    internal class GradeStatistics
+    {
+        float averageGrade;
+        float highestGrade;
+        float lowestGrade;
+
+        public float AverageGrade
+        {
+            get
+            {
+                return averageGrade;
+            }
+            set
+            {
+                averageGrade = value;
+            }
+        }
+        public float HighestGrade
+        {
+            get
+            {
+                return highestGrade;
+            }
+            set
+            {
+                highestGrade = value;
+            }
+        }
+        public float LowestGrade
+        {
+            get
+            {
+                return lowestGrade;
+            }
+            set
+            {
+                lowestGrade = value;
+            }
+        }
+
+
+    }
+}
